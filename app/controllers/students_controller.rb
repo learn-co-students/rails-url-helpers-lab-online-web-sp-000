03866445a4409f7b26dd4ec97b5adbe880db016a
Becky Phan <beckyphan@live.com>
2019-09-19 01:01:33 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,24 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    set_student
+  end
+
+  def activate
+    set_student
+    
+    if @student.active == true
+      @student.update(active: false)
+    else
+      @student.update(active: true)
+    end
+
+    redirect_to @student
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :students, only: [:index, :show]
+
+  get 'students/:id/activate', to: 'students#activate', as: 'activate_student'
+
 end
+
+# Prefix    Verb  URI Pattern                     Controller#Action
+# students  GET  /students(.:format)              students#index
+# student   GET  /students/:id(.:format)          students#show
+#           GET  /students/:id/activate(.:format) students#activate
