229af1b1366981af881daf79b4f95d90ddfd3f89
Andrew-J-Williams <1williamsaj@gmail.com>
2020-03-07 14:59:07 -0600

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,6 +6,14 @@ class StudentsController < ApplicationController
   end
 
   def show
+    @students = Student.find(params[:id])
+  end
+
+  def activate
+    @student = Student.find(params[:id]) #find our specific student
+    @student.active = !@student.active #set our status to the opposite of whatever it currently is (ex. true would become false)
+    @student.save #We save the paramters for this student
+    redirect_to student_path(@student) #finally, redirect the user back to the student's show page
   end
 
   private
