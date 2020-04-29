55c42b63e9d350cbef4f59418f7518e5bbadcd08
Ryan Mui <reachryanmui@gmail.com>
2020-04-29 00:54:50 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,11 +1,16 @@
 class Student < ActiveRecord::Base
 
+
+  # @active = false
   def to_s
     self.first_name + " " + self.last_name
   end
   def display_active
     active ? "active" : "inactive"
   end
+  # def is_active
+  #   self.active ? "active" : "inactive"
+  # end
 end
 
 # drop database
