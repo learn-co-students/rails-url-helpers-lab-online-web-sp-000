7703f53d0a7291469eeb1269d58c976d23a2aa4d
nathan-stamand <nathansaint810@gmail.com>
2020-05-05 07:34:33 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,11 +6,23 @@ class StudentsController < ApplicationController
   end
 
   def show
+    set_student
+  end
+
+  def edit 
+    set_student
+    @student.active = !@student.active
+    @student.save
+    redirect_to student_path(@student)
   end
 
   private
 
     def set_student
       @student = Student.find(params[:id])
+    end
+
+    def to_s 
+      self.first_name + " " + self.last_name
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "students" => "students#index", as: 'students'
+  get "students/:id" => "students#show", as: 'student'
+  get "students/:id/edit" => "students#edit", as: 'activate_student'
 end
