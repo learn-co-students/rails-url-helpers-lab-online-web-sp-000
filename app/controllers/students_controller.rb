da18a74f0eb00a511b7afed45129a952396bcf7f
Dan-zai <danielcowpertwait@gmail.com>
2020-05-28 14:09:32 +1000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,12 +1,20 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    @student = Student.find(params[:id])
   end
+
+  def activate
+    @student = Student.find(params[:id])
+    @student.toggle(:active).save
+    #dope little method but... does it skip validation?
+    redirect_to student_path(@student)
+  end 
 
   private
 
