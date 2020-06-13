47639071a21e5a8d7fb4516e4d40aad52d32c1ef
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-06-13 12:42:28 +0200

    Done.

---
+++ db/migrate/002_adding_active_to_students.rb
@@ -0,0 +1,8 @@
+class AddingActiveToStudents < ActiveRecord::Migration
+    def change
+      change_table :students do |t|
+        t.boolean :active, :default => false
+        
+      end
+    end
+end
