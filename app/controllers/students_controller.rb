818c8db842d66efa64170078aa63a73d1ba52c70
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-07-05 16:07:33 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,10 +4,24 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
-
+  
   def show
-  end
+    @student = Student.find(params[:id])
+  end 
 
+  def activate
+    @student = Student.find(params[:id])
+
+    if @student.active
+       @student.active = false
+     else
+       @student.active = true
+    end
+    @student.active
+    @student.save
+
+    redirect_to student_path(@student)
+  end
   private
 
     def set_student
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.create(first_name: 'Ryan', last_name: "Tierney")
+Student.create(first_name: 'Derek', last_name: "Kerley")
