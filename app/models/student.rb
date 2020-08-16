353f9f9fc11468a8ce0f6e4f1ab24f93dbf3c5d6
satodd1967 <seth.todd@outlook.com>
2020-08-15 19:10:44 -0500

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,21 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def active_inactive
+    if self.active == false
+      return "inactive"
+    else
+      return "active"
+    end
+  end
+
+  def active_switch
+    if self.active == false
+      self.active = true
+    else
+      self.active = false
+    end
+  end
+
 end
