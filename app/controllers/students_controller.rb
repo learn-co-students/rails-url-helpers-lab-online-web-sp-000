c19d6ba93daee38365106556bb5319691d94fd0d
Alexander M. Clamor <alexander.clamor@gmail.com>
2020-10-28 03:18:21 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -20,7 +20,7 @@ class StudentsController < ApplicationController
     student = set_student
     student.active = !student.active
     student.save
-    binding.pry
+
     redirect_to student_path
   end
 
