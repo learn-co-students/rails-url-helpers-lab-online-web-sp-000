99255653d1718d8734c1e0b83c2448836197dde3
rmholsen <jaguar.kitty@gmail.com>
2021-01-25 21:58:53 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,7 +11,9 @@ class StudentsController < ApplicationController
   def activate
     @student = Student.find(params[:id])
     @student.active = !@student.active 
+    #No idea why this works and the if/else AND the simple bang operator didn't work but sure why not. 
     @student.save 
+    #STOP FORGETTING TO SAVE YOUR UPDATES.
     redirect_to "/students/#{@student.id}"
   end 
 
