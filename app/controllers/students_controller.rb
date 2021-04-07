73cbc31b5a029ede7547a03fd831814f841fb4b6
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-04-07 15:05:27 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,15 +1,22 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
+  include ApplicationHelper
   
   def index
     @students = Student.all
   end
 
   def show
+    set_student
+  end
+
+  def activate
+    @student = set_student
+    @student.toggle!(:active)
+    redirect to student_path(@student)
   end
 
   private
-
     def set_student
       @student = Student.find(params[:id])
     end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def active?
+        @student = set_student
+        if @student.active == "true"
+          puts "active"
+        else
+          puts "inactive"
+        end
+      end
 end
