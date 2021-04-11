fd44a142e07dd5e2d1cfc64cd4482212810cd4ec
bczorn <bczorn@gmail.com>
2021-04-10 20:38:49 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :students, only: [:index, :show]
+  # get "students/:id/activate", to: redirect('students/show/%{id}')
+  get '/students/:id/activate', to: 'students#activate', as: :activate_student
+  get '/students/:id/activate' => redirect(path: '/students/:id')
+  # match '/students/:id/activate' => redirect('/students/%{id}')
 end
